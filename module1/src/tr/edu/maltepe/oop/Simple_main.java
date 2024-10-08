84205dc12cb42a6e3eea83fe52941b153d5f5058
Berru Hanedar <berruhanedar@icloud.com>
2024-10-08 09:32:46 +0300

    initial commit

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,10 +1,10 @@
 package tr.edu.maltepe.oop;
-public  class imple_main {
 
- public static void main(String []args){
-        //
-       System.out.println("simple main.....");
-        }
+public class Simple_main {
+
+    public static void main(String[] args) {
+        System.out.println("simple main....Today date is 2024-10-8.");
+    }
 }
 
 
